66e0bc3c1902048820dfce9ec23cf464feef2250
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-09-21 18:10:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
      "Fizz"
+   elsif int % 5 == 0
+     "Buzz"
+   else
+     "Fizzbuzz"
    end
-
- def fizzbuzz(int)
-  if int % 5 == 0
-    "Buzz"
-  end
  end
